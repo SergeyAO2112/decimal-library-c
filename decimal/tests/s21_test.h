82cef0ad7ec7eda8965471b4140a1ef6a322c1ec
Sergey Alekseev <158785461+SergeyAO2112@users.noreply.github.com>
2026-03-18 13:53:50 +0300

    Update s21_test.h

--- decimal/tests/s21_test.h
+++ decimal/tests/s21_test.h
@@ -1,1 +1,12 @@
+#ifndef S21_TESTS_DECIMAL
+#define S21_TESTS_DECIMAL
+#include <check.h>
 
+#include "../s21_decimal.h"
+
+Suite *convert_tests_suite(void);
+Suite *other_tests_suite(void);
+Suite *arithmetic_tests_suite(void);
+Suite *compare_tests_suite(void);
+
+#endif
